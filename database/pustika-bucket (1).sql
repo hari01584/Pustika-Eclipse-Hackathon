02889c709aaac048a1fdf2994330331828a2a9ae
RISHI AWASTHI <73848973+RISHIAWASTHI@users.noreply.github.com>
2020-11-22 01:12:22 +0530

    All images added

--- database/pustika-bucket (1).sql
+++ database/pustika-bucket (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2020 at 06:45 PM
+-- Generation Time: Nov 21, 2020 at 08:41 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -103,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `picture` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `products`
@@ -120,7 +120,29 @@ INSERT INTO `products` (`id`, `pName`, `price`, `description`, `available`, `cat
 (40, 'H.S. Kasana book for Complex Variables', 350, 'Book is of latest version and has good condition', 1, 'sub02', 'Book', 'UMA014', 'H.S._Kasana_book_for_Complex_Variables.jpg', '2020-11-21 17:18:07'),
 (41, 'Hughes Book for Electrical Engineering', 325, 'Book is in good condition and has all topics covered', 1, 'sub03', 'Book by Hughes', 'UES011', 'Hughes_electricalengineering.jpg', '2020-11-21 17:22:35'),
 (42, 'Nagrath Kothari Book for Electrical Engineering', 299, 'Book is in a good condition and is useful for B.Tech course', 1, 'sub03', 'Writer-Nagrath Kothari', 'UES011', 'Nagrath_Kothari_electrical_engineering.jpg', '2020-11-21 17:24:24'),
-(43, 'NK Verma Applied Physics', 300, 'Book is really good in its condition and have very basic topics also covered', 1, 'sub03', 'Book for Applied Physics', 'UES016', 'NK_Verma_Applied_Physics.jpg', '2020-11-21 17:26:09');
+(43, 'NK Verma Applied Physics', 300, 'Book is really good in its condition and have very basic topics also covered', 1, 'sub03', 'Book for Applied Physics', 'UES016', 'NK_Verma_Applied_Physics.jpg', '2020-11-21 17:26:09'),
+(44, 'Fundamental of Electrical Circuit by Charles Alexander', 300, 'Book is in a good condition', 1, 'sub03', 'Book', 'UES005', 'Fundamental_of_electrical_circuit_by_Charles_alexander.jpg', '2020-11-21 17:56:19'),
+(45, 'Engineering Circuit analysis by William Hayt', 299, 'Book is in good condition', 1, 'sub03', 'Extra ', 'UES002', 'Engineering_circuit_analysis_by_william_hayt.jpg', '2020-11-21 17:59:50'),
+(46, 'Statics and Dynamics by R.C. Hibbler Mechanics', 325, 'Book is good for beginners and is in good condition', 1, 'sub04', 'Book', 'UES001', 'R.C._Hibbler_Mechanics.jpg', '2020-11-21 18:04:25'),
+(47, 'V.Raghavan Engineering Materials', 310, 'Book is in good condition', 1, 'sub04', 'Engineering material', 'UES005', 'V.Raghavan_Engineering_Materials.jpg', '2020-11-21 18:05:52'),
+(48, 'I.H. Shames Engineering Mechanics:Statics and Dynamics', 350, 'Book is really good for mechanics and have good problems', 1, 'sub04', 'Mechanics', 'UES008', 'Shames_Engineering_Mechanics.jpg', '2020-11-21 18:09:29'),
+(49, 'A. Nelson Mechanics Statics and Dynamics', 299, 'Book is good and covers basic concept', 1, 'sub04', 'Book', 'UES010', 'A_Nelson_Mechanics_Statics_and_Dynamics.jpg', '2020-11-21 18:13:43'),
+(52, 'Engineering Chemistry By Shikha Aggarwal', 310, 'Book is in good condition', 1, 'sub05', 'Chemistry Book', 'UCB002', 'Engineering_Chemistry_By_Shikha_Aggarwal.jpg', '2020-11-21 18:20:37'),
+(53, 'Engineering-Chemistry-Dr HG Bhatt', 310, 'Book is in good condition', 1, 'sub05', 'Book', 'UCB025', 'Engineering-Chemistry-Dr_HG_Bhatt.png', '2020-11-21 18:50:31'),
+(54, 'Air-Pollution-HVN Rao', 299, 'Book is in good condition', 1, 'sub06', '1', 'UEN001', 'Rao-H-V-N-Rao-Air-Pollution.jpg', '2020-11-21 18:53:29'),
+(55, 'Introduction to environmental engineerind and science by Gilbert M Masters', 300, 'Book is in good condition', 1, 'sub06', '1', 'UEN025', 'Introduction_to_environmental_engineerind_and_science_by_Gilbert_M_Masters.jpg', '2020-11-21 19:00:11'),
+(56, 'A software engineer by Roger S. Pressman', 325, 'Book is in good condition with really good content', 1, 'sub07', 'Book', 'UVE011', 'A_software_engineer_by_Roger_S._Pressman.jpg', '2020-11-21 19:12:03'),
+(57, 'Computer System Architecture by M. Morris Mano', 340, 'Book is of great use it covers all topics', 1, 'sub07', '1', 'UCV001', 'computer_system_architecture_by_M._Morris_Mano.jpg', '2020-11-21 19:13:26'),
+(58, 'Data Mining Concept and Techniques by Jiawei Han', 328, 'Book is in good condition', 1, 'sub07', 'Book for electives', 'UED001', 'data_mining_concept_and_techniques_by_Jiawei_Han.jpg', '2020-11-21 19:15:19'),
+(59, 'Principle of Corporate Finance by Brealey', 300, 'Book is good for Corporate finance topic and is in good condition', 1, 'sub07', 'Book', 'UCA001', 'Principle_of_corporate_finance_by_Brealey.jpeg', '2020-11-21 19:16:42'),
+(60, 'Mathematics by Manan Khurama', 300, 'Book is good for JEE preparation', 1, 'sub08', 'Book for competition', 'UBN001', 'Mathematics_by_Manan_Khurama.jpg', '2020-11-21 19:19:04'),
+(61, 'Pathfinder for Olympiad & JEE Physics by Arvind Tiwari', 340, 'Book is in good condition', 1, 'sub08', 'By Arvind Tiwari', 'UBA002', 'Pathfinder_for_Olympiad_&_JEE_Physics_by_Arvind_Tiwari.jpg', '2020-11-21 19:21:40'),
+(62, 'A Textbook of Organic Chemistry for JEE Main and Advanced by Dr RK Gupta', 320, 'Book is good for JEE preparations', 1, 'sub08', '1', 'UCA-005', 'A_Textbook_of_Organic_Chemistry_for_JEE_Main_and_Advanced_by_Dr_RK_Gupta.jpg', '2020-11-21 19:23:32'),
+(63, 'Book on aptitude by R.K. Aggarwal', 325, 'Book is in good condition', 1, 'sub08', 'Book', 'UCA-008', 'Book_on_aptitude_by_R.K._Aggarwal.jpg', '2020-11-21 19:25:31'),
+(64, 'Zero to One by Peter Thiel', 250, 'Book is in good condition', 1, 'sub09', '1', 'UAB001', 'Zero_to_One.jpg', '2020-11-21 19:32:58'),
+(65, 'Rich Dad and Poor Dad by Robert', 300, 'Book is in good condition', 1, 'sub09', 'Book', 'UAB004', 'Rich_Dad_and_Poor_Dad_by_Robert.jpg', '2020-11-21 19:36:02'),
+(66, 'The Power of Positive Thinking by Norman Vincent', 290, 'Book is in good condition', 1, 'sub09', '1', 'UAB-005', 'The_Power_of_Positive_Thinking_by_Norman_Vincent.jpg', '2020-11-21 19:38:04'),
+(67, 'The Power of Positive Attitude by Fredrick Douglas', 249, 'Book is in good condition', 1, 'sub09', 'Book', 'UAB008', 'The_Power_of_Positive_Attitude_by_Fredrick_Douglas.jpg', '2020-11-21 19:41:00');
 
 -- --------------------------------------------------------
 
@@ -143,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `product_level` (
   `converse` varchar(10) NOT NULL DEFAULT 'no',
   `loafer` varchar(10) NOT NULL DEFAULT 'no',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `product_level`
@@ -191,7 +213,31 @@ INSERT INTO `product_level` (`id`, `product_id`, `v_shape`, `polo`, `clean_text`
 (40, 40, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
 (41, 41, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
 (42, 42, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
-(43, 43, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no');
+(43, 43, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(44, 44, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(45, 45, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(46, 46, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(47, 47, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(48, 48, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(49, 49, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(50, 50, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(51, 51, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(52, 52, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(53, 53, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(54, 54, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(55, 55, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(56, 56, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(57, 57, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(58, 58, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(59, 59, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(60, 60, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(61, 61, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(62, 62, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(63, 63, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(64, 64, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(65, 65, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(66, 66, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(67, 67, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no');
 
 -- --------------------------------------------------------
 
