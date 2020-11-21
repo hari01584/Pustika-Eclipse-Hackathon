d2e8103680a4f8efffc7939491aa6ebc9d29d826
RISHI AWASTHI <73848973+RISHIAWASTHI@users.noreply.github.com>
2020-11-21 23:19:05 +0530

    Photos added

--- database/pustika-bucket.sql
+++ database/pustika-bucket.sql
@@ -1,21 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 21, 2020 at 04:12 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2020 at 06:45 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `pustika-bucket`
@@ -27,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(125) NOT NULL,
@@ -39,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `type` varchar(20) NOT NULL,
   `confirmCode` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `admin`
@@ -56,7 +54,6 @@ INSERT INTO `admin` (`id`, `firstName`, `lastName`, `email`, `mobile`, `address`
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) DEFAULT NULL,
@@ -69,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `odate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ddate` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `orders`
@@ -94,7 +91,6 @@ INSERT INTO `orders` (`id`, `uid`, `ofname`, `pid`, `quantity`, `oplace`, `mobil
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pName` varchar(100) NOT NULL,
@@ -107,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `picture` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
 
 --
 -- Dumping data for table `products`
@@ -118,7 +114,13 @@ INSERT INTO `products` (`id`, `pName`, `price`, `description`, `available`, `cat
 (28, 'C++ by E.Balaguruswamy', 300, 'Good condition book of previous session', 1, 'sub01', 'book', 'UTA043', 'ebalaswami.jpg', '2020-11-21 14:49:53'),
 (29, 'C Programming for Arduino by Julien Bayle', 350, 'Good condition Book', 1, 'sub01', 'Julien Bayle', 'UTA004', 'C_programming_for_arduino_Julien_Bayle.jpg', '2020-11-21 14:58:21'),
 (30, 'Introduction to Algorithms by Thomas H. Corem', 399, 'Book for Beginners and in good condition', 1, 'sub01', 'Thomas H. Corem', 'UTA020', 'Introduction_to_algorithms_by_Thomas_H._Corem.jpg', '2020-11-21 15:08:45'),
-(37, 'Mathematics for finance by Marek Capinski', 300, 'Good book with all basic topics covered', 1, 'sub02', 'Book', 'UED002', 'mathematics_for_finance_by_Marek_Capinski.jpg', '2020-11-21 15:44:17');
+(37, 'Mathematics for finance by Marek Capinski', 300, 'Good book with all basic topics covered', 1, 'sub02', 'Book', 'UED002', 'mathematics_for_finance_by_Marek_Capinski.jpg', '2020-11-21 15:44:17'),
+(38, 'Thomas and Finney Calculus and DifferentialEquation', 299, 'Book is in a good condition and have all topics covered', 1, 'sub02', 'Thomas and Finney', 'UMA010', 'Thomas_and_Finney_Calculus.jpg', '2020-11-21 17:12:32'),
+(39, 'Mathematics for Engineers by Anthony Croft', 310, 'Book is in a good condition and has perfect pages', 1, 'sub02', 'Book', 'UMA011', 'Mathematics_for_Engineers_by_Anthony_Croft.jpg', '2020-11-21 17:15:26'),
+(40, 'H.S. Kasana book for Complex Variables', 350, 'Book is of latest version and has good condition', 1, 'sub02', 'Book', 'UMA014', 'H.S._Kasana_book_for_Complex_Variables.jpg', '2020-11-21 17:18:07'),
+(41, 'Hughes Book for Electrical Engineering', 325, 'Book is in good condition and has all topics covered', 1, 'sub03', 'Book by Hughes', 'UES011', 'Hughes_electricalengineering.jpg', '2020-11-21 17:22:35'),
+(42, 'Nagrath Kothari Book for Electrical Engineering', 299, 'Book is in a good condition and is useful for B.Tech course', 1, 'sub03', 'Writer-Nagrath Kothari', 'UES011', 'Nagrath_Kothari_electrical_engineering.jpg', '2020-11-21 17:24:24'),
+(43, 'NK Verma Applied Physics', 300, 'Book is really good in its condition and have very basic topics also covered', 1, 'sub03', 'Book for Applied Physics', 'UES016', 'NK_Verma_Applied_Physics.jpg', '2020-11-21 17:26:09');
 
 -- --------------------------------------------------------
 
@@ -126,7 +128,6 @@ INSERT INTO `products` (`id`, `pName`, `price`, `description`, `available`, `cat
 -- Table structure for table `product_level`
 --
 
-DROP TABLE IF EXISTS `product_level`;
 CREATE TABLE IF NOT EXISTS `product_level` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
@@ -142,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `product_level` (
   `converse` varchar(10) NOT NULL DEFAULT 'no',
   `loafer` varchar(10) NOT NULL DEFAULT 'no',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
 
 --
 -- Dumping data for table `product_level`
@@ -184,7 +185,13 @@ INSERT INTO `product_level` (`id`, `product_id`, `v_shape`, `polo`, `clean_text`
 (34, 34, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
 (35, 35, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
 (36, 36, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
-(37, 37, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no');
+(37, 37, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(38, 38, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(39, 39, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(40, 40, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(41, 41, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(42, 42, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no'),
+(43, 43, 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no');
 
 -- --------------------------------------------------------
 
@@ -192,14 +199,13 @@ INSERT INTO `product_level` (`id`, `product_id`, `v_shape`, `polo`, `clean_text`
 -- Table structure for table `product_view`
 --
 
-DROP TABLE IF EXISTS `product_view`;
 CREATE TABLE IF NOT EXISTS `product_view` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `product_view`
@@ -223,7 +229,6 @@ INSERT INTO `product_view` (`id`, `user_id`, `product_id`, `date`) VALUES
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
@@ -235,7 +240,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `online` varchar(1) NOT NULL DEFAULT '0',
   `activation` varchar(3) NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `users`
@@ -244,7 +249,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `mobile`, `reg_time`, `online`, `activation`) VALUES
 (20, 'Admin', 'supersu@s', 'supersu', '$5$rounds=535000$oAqAfLqFXt.vR.oR$e8glznWrzA5BclBSpBJnSHJNk2DhYVuTE7Gd/NRCXw9', '+00000000000', '2020-11-21 12:48:22', '0', 'yes'),
 (19, 'Harishankar Kumar', 'hari01584@gmail.com', 'hari01584', '$5$rounds=535000$/aZcgCuhLQo8Lle5$IskgTI..PWhJAzN/R0KFriecscbvdEffH5HfxBOuE/1', '+918872276957', '2020-11-21 03:57:29', '1', 'yes');
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
